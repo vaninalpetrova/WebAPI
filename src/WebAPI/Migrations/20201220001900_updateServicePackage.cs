b246683b6568f2c537466b497c7bb5d3fd5cf151
VaninaPetrova <63479504+vaninalpetrova@users.noreply.github.com>
2020-12-20 13:34:55 +0200

    Create 20201220001900_updateServicePackage.cs

---
+++ src/WebAPI/Migrations/20201220001900_updateServicePackage.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPI.Migrations
+{
+    public partial class updateServicePackage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
