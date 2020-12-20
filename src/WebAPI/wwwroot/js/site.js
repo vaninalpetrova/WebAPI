efe0d9fabf981d7a91c26540dff1de1f17df14ab
VaninaPetrova <63479504+vaninalpetrova@users.noreply.github.com>
2020-12-20 14:50:27 +0200

    Create site.js

---
+++ src/WebAPI/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(function () {
+    const yearField = $('#Year');
+
+    if (yearField) {
+        let date = yearField.attr('data-date');
+
+        //date = new Date(date);
+        yearField.val(date);
+    }
+
+});
