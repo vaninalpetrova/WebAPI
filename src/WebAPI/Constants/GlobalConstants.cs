b52dfddb23fd675592306ef215d922cb7e83c6d9
VaninaPetrova <63479504+vaninalpetrova@users.noreply.github.com>
2020-12-20 13:05:53 +0200

    Create GlobalConstants.cs

---
+++ src/WebAPI/Constants/GlobalConstants.cs
@@ -0,0 +1,11 @@
+namespace WebAPI.Constants
+{
+    public static class GlobalConstants
+    {
+        public const string AdminArea = "Admin";
+        public const string AdminRole = "Administrator";
+
+        public const string UserArea = "NormalUser";
+        public const string UserRole = "User";
+    }
+}
