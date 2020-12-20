b68c1dd6e5545a8bfc690bb75e49a4990d5504c3
VaninaPetrova <63479504+vaninalpetrova@users.noreply.github.com>
2020-12-20 13:30:22 +0200

    Create ServicePackageConfiguration.cs

---
+++ src/WebAPI/Data/EntityTypeConfigurations/ServicePackageConfiguration.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Data.DataModels;
+
+namespace WebAPI.Data.EntityTypeConfigurations
+{
+    public class ServicePackageConfiguration : IEntityTypeConfiguration<ServicePackage>
+    {
+        public void Configure(EntityTypeBuilder<ServicePackage> builder)
+        {
+            builder.HasOne(sp => sp.User)
+                .WithMany(u => u.ServicePackages)
+                .HasForeignKey(sp => sp.UserId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.Property(sp => sp.Price)
+                .HasColumnType("DECIMAL(15, 2)");
+        }
+    }
+}
